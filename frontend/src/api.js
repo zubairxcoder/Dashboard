993d567f9088e81986133e84950d3fcb6237fd36
zubaircoderx <zubaircoderx@gmail.com>
2026-05-17 01:00:48 +0500

    Update API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: '/_/backend/api'
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
@@ -15,6 +21,7 @@ api.interceptors.response.use(
       localStorage.removeItem('token');
       window.location.href = '/login';
     }
+
     return Promise.reject(err);
   }
 );
